853fa543e9763a824ce0afb4bbb3e888f0bee0d7
Garry Trinder <garry.trinder@live.com>
2015-12-13 21:59:01 +0000

    Added socket.io and onoff npm packages Wired up socket.io, server and client passing messages. Starts with LED turned on as default.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
+app.io = require('socket.io')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -36,7 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -47,7 +48,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -55,5 +56,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var GPIO = require('onoff').Gpio;
+var led = new GPIO(18, 'out');
+var button = new GPIO(17, 'in', 'both');
+
+app.io.on('connection', function (socket) {
+	socket.emit('led', { led: 1 });
+});
 
 module.exports = app;
---
+++ public/javascripts/led.js
@@ -0,0 +1,27 @@
+/* global jQuery */
+/* global io */
+var socket = io();
+var $button = jQuery('#onoff');
+
+$button.on('click', function (e) {
+	var state = $button.data('led');
+	if (state === 'on') {
+		socket.emit('led', { led: 0 });
+		$button.data('led','off');
+		$button.text('On');
+	} else {
+		socket.emit('led', { led: 1 });
+		$button.data('led','on');
+		$button.text('Off');		
+	}
+});
+
+socket.on('led', function (data) {
+	if (data.led === 1) {
+		$button.data('led', 'on');
+		$button.text("Off");
+	} else {
+		$button.data('led', 'off');
+		$button.text("On");
+	}
+});
